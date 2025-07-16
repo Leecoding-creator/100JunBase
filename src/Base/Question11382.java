82a4de3c90f0e6088c42ff55285f4c99d9526904
Lee Jun Ho <wmfm314212@gmail.com>
2025-07-16 23:40:42 +0900

    done

---
+++ src/Base/Question11382.java
@@ -0,0 +1,12 @@
+package Base;
+import java.util.Scanner;
+
+public class Question11382 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long A=sc.nextLong(), B=sc.nextLong(), C=sc.nextLong();
+		//(1 ≤ A, B, C ≤ 10^12)
+		System.out.println(A+B+C);
+		sc.close();
+	}
+}
