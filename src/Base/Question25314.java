22b4b538f81e6466012f4add8a0589928ca626b3
Lee Jun Ho <wmfm314212@gmail.com>
2025-08-17 10:17:12 +0900

    chep03

---
+++ src/Base/Question25314.java
@@ -0,0 +1,16 @@
+package chep03;
+
+import java.util.Scanner;
+
+public class Question25314 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		for(int i=0; i<N/4; i++)
+			System.out.print("long ");
+		System.out.println("int");
+		
+		sc.close();
+	}
+}
