693717957bb412f8838283810ddab485d7f76e0a
Lee Jun Ho <wmfm314212@gmail.com>
2025-08-20 09:21:36 +0900

    chep03

---
+++ src/Base/Question2438.java
@@ -0,0 +1,19 @@
+package chep03;
+
+import java.util.Scanner;
+
+public class Question2438 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		
+		for(int i=0; i<a; i++) {
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		sc.close();
+	}
+}
