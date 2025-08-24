58b3b7139e45f0a79bf38b7c7c5994ce1b4e747a
Lee Jun Ho <wmfm314212@gmail.com>
2025-08-24 23:41:18 +0900

    chep03

---
+++ src/Base/Question10951.java
@@ -0,0 +1,17 @@
+package chep03;
+
+import java.util.Scanner;
+
+public class Question10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextInt()) { // 입력이 더 이상 없을 때까지 반복
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B);
+        }
+
+        sc.close();
+    }
+}
