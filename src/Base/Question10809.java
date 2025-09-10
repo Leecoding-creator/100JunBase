563e13ef0228d1034c153814267a4cc2e066dd6a
Lee Jun Ho <wmfm314212@gmail.com>
2025-09-10 10:50:06 +0900

    chep05

--- src/Base/Question10809.java
+++ src/Base/Question10809.java
@@ -3,16 +3,27 @@ package chep05;
 import java.util.Scanner;
 
 public class Question10809 {
+
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), sum = 0;
-        String str = sc.next();
-        
-        for(int i=0; i<n; i++) {
-            sum += str.charAt(i) - '0'; // ex) '3' - '0' -> 51 - 48
-        }
-        
-        System.out.println(sum);
-        sc.close();
-    }
+		Scanner sc = new Scanner(System.in);
+		
+		int[] index = new int[26];
+		for(int i=0; i<index.length; i++) {
+			index[i] = -1;
+		}
+		
+		String str = sc.next();
+		
+		for(int i=0; i<str.length(); i++) {
+			if(index[(str.charAt(i)- 'a')] == -1) {
+				index[(str.charAt(i)- 'a')] = i;
+			}
+		}
+		
+		for(int i=0; i<index.length; i++) {
+			System.out.printf("%d ", index[i]);
+		}
+		sc.close();
+	}
+
 }
