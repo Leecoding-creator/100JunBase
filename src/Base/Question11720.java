96802e407fba762da3e341ae3ac012458479d832
Lee Jun Ho <wmfm314212@gmail.com>
2025-09-13 01:49:50 +0900

    chep05

---
+++ src/Base/Question11720.java
@@ -0,0 +1,18 @@
+package chep05;
+
+import java.util.Scanner;
+
+public class Question11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), sum = 0;
+        String str = sc.next();
+        
+        for(int i=0; i<n; i++) {
+            sum += str.charAt(i) - '0';
+        }
+        
+        System.out.println(sum);
+        sc.close();
+    }
+}
