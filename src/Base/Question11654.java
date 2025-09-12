63ea83868ee7643a038c1859a447878fa2aee25f
Lee Jun Ho <wmfm314212@gmail.com>
2025-09-12 22:33:51 +0900

    chep05

---
+++ src/Base/Question11654.java
@@ -0,0 +1,12 @@
+package chep05;
+import java.util.Scanner;
+
+public class Question11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char c = sc.next().charAt(0);
+		int ch = (int)c;
+		System.out.println(ch);
+		sc.close();
+	}
+}
