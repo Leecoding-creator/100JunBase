934e414b58e31583f1fb03b1065cf7b653992cc2
Lee Jun Ho <wmfm314212@gmail.com>
2025-11-22 10:12:11 +0900

    chep11

---
+++ src/chapter10-19/Question24265.java
@@ -0,0 +1,13 @@
+package chep11;
+
+import java.util.Scanner;
+
+public class Question24265 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		sc.close();
+		System.out.println(n*(n-1)/2);
+		System.out.println(2);
+	}
+}
